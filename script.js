9cf00f2590494646b12adf03d7f2f6e15c24662c
Wadey_56 <126613022+Wadey56@users.noreply.github.com>
2024-08-01 21:12:14 +0100

    Limit returns to 7 decimals

--- script.js
+++ script.js
@@ -1,27 +1,29 @@
+// .toFixed(7)
+
 function add(number1, number2) {
     if (isNaN(number1) || isNaN(number2)) {
         return;
     }
-    return number1 + number2;
+    return parseFloat((number1 + number2).toFixed(7));
 }
 
 function subtract(number1, number2) {
     if (isNaN(number1) || isNaN(number2)) {
         return;
     }
-    return number1 - number2;
+    return parseFloat((number1 - number2).toFixed(7));
 }
 
 function multiply(number1, number2) {
     if (isNaN(number1) || isNaN(number2)) {
         return;
     }
-    return number1 * number2;
+    return parseFloat((number1 * number2).toFixed(7));
 }
 
 function divide(number1, number2) {
     if (isNaN(number1) || isNaN(number2)) {
         return;
     }
-    return number1 / number2;
+    return parseFloat((number1 / number2).toFixed(7));
 }
