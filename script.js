03b8d5ec89833e2afc4666e7b5bd6802e6b40e17
Wadey_56 <126613022+Wadey56@users.noreply.github.com>
2024-08-01 18:38:23 +0100

    Handle NaN entries

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
 function add(number1, number2) {
+    if (isNaN(number1) || isNaN(number2)) {
+        return;
+    }
     return number1 + number2;
 }
 
 function subtract(number1, number2) {
+    if (isNaN(number1) || isNaN(number2)) {
+        return;
+    }
     return number1 - number2;
 }
 
 function multiply(number1, number2) {
+    if (isNaN(number1) || isNaN(number2)) {
+        return;
+    }
     return number1 * number2;
 }
 
 function divide(number1, number2) {
+    if (isNaN(number1) || isNaN(number2)) {
+        return;
+    }
     return number1 / number2;
 }
